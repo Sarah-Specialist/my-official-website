225cca6249851558981f63c25a6e8a392cc96a76
Sarah Specialist <sarah.specialist@gmail.com>
2022-01-29 07:27:58 +0800

    edited styling

--- official-website/src/parts/logo/logo.js
+++ official-website/src/parts/logo/logo.js
@@ -4,7 +4,7 @@ export default function Logo() {
 
     return (
         <div className="logoDiv">
-            <img src="" alt="logo" />
+            <img src="https://i.ibb.co/DY7Ty5B/logo.jpg" alt="logo" className="logo" />
         </div>
     )
 }
