8a581aba412223cca072362a8aa090282c98a5e9
Sarah Specialist <sarah.specialist@gmail.com>
2022-01-28 18:55:54 +0800

    added header and footer files

--- official-website/src/App.js
+++ official-website/src/App.js
@@ -1,9 +1,16 @@
+import React from 'react';
+import Header from './parts/header/header';
+import Footer from './parts/footer/footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <div className="Container">
+        <Header />
+        <Footer />
+        Hello world
+      </div> 
     </div>
   );
 }
