0fa4980a0606f308d37f5bc5259f585b8e6e8377
Sarah Specialist <sarah.specialist@gmail.com>
2022-01-29 06:42:59 +0800

    added logo and footer

--- official-website/src/parts/footer/footer.js
+++ official-website/src/parts/footer/footer.js
@@ -4,7 +4,28 @@ export default function Footer() {
 
     return (
         <div className="footer">
-            Footer
+            <span className="copyright">Sarah Khoo</span>
+            <div className="socials">
+            <img src="https://cdn-icons-png.flaticon.com/512/124/124010.png" 
+                alt="facebook logo" 
+                className="social"
+                style= {{ height: 20, width:20, backgroundColor: "white"}} />  
+
+                <img src="https://cdn-icons-png.flaticon.com/128/1409/1409946.png" 
+                alt="instagram logo" 
+                className="social"
+                style= {{ height: 20, width:20, backgroundColor: "white"}} />  
+
+            <img src="https://cdn-icons.flaticon.com/png/128/3536/premium/3536505.png?token=exp=1643409937~hmac=dc677e23e6cdd2332fe373d4caee6c77" 
+                alt="linkedin logo" 
+                className="social"
+                style= {{ height: 20, width:20, backgroundColor: "white"}} /> 
+
+            <img src="https://cdn-icons.flaticon.com/png/128/3291/premium/3291695.png?token=exp=1643409894~hmac=7c36ddeff3320bab591ac3c99a57d404" 
+                alt="github logo" 
+                className="social"
+                style= {{ height: 20, width:20, backgroundColor: "white"}} /> 
+            </div>
         </div>
     )
 }
