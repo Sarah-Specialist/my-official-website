71ef5af85e808a67caf4a9cd27cd18e1cbd4f1fc
Sarah Specialist <sarah.specialist@gmail.com>
2022-01-29 12:08:31 +0800

    edited about page

--- official-website/src/pages/about/about.js
+++ official-website/src/pages/about/about.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './about.css';
 
 export default function About() {
     return (
-        <>
+        <div className="about">
+            <h3>About Me</h3>
             <p>I decided to give it a shot, and never looked back!</p>
             <p>Growing up, I’ve always had a knack for Science and pursued my passion for the Healthcare industry. As part of the medical ICU team, I’ve worked in high-pressure work environment and contributed as a front-line healthcare worker during the COVID-19 pandemic. I will always retain a strong interest toward Healthcare, and I cannot wait to merge those experiences with new skills that I am learning everyday.</p>
             <p>I’ve had an interest in coding for a long time, and I used to spend my free time attending courses and reading up. I am currently enrolled in the NTU Software Developer Immersive course, and I’m learning a lot from wonderful mentors and classmates! The curriculum for this course include programming fundamentals, frontend framework and APIs, backend framework and APIs, mobile and full-stack development, relational and noSQL databases, and agile and product development.</p>
-        </>
+        </div>
     )
 }
