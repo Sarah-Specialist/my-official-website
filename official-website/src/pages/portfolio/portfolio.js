adccd3f98c1cf42fc525a89a0dc847010fa6b834
Sarah Specialist <sarah.specialist@gmail.com>
2022-01-30 17:20:03 +0800

    added content in portfolio

--- official-website/src/pages/portfolio/portfolio.js
+++ official-website/src/pages/portfolio/portfolio.js
@@ -13,7 +13,7 @@ export default function Portfolio() {
       articleGoal="Goal: This tool helps physicians understand the patient’s lung condition and oxygenation status. This is typically used for patients with acute respiratory distress syndrome (ARDS)."
       articleLF="Languages and Frameworks: HTML, CSS, JavaScript, draw.io, Visual Studios Code, GitHub"
       articleFeatures="Features: input validation, mathematics calculation, mouseover event, radio buttons that autofill input text"
-      articleProcess="Process: While thinking of a topic for my project, I was motivated to solve a problem that is commonly faced by junior doctors - being unsure of the amount of oxygen that a particular mask/device is providing. This tool allows physicians to directly select the device and have the FiO2 field auto-filled."
+      articleProcess="Process: While thinking of a topic for my project, I was motivated to solve a problem that is commonly faced by junior doctors - being unsure of the amount of oxygen that a particular mask/device is providing. This tool allows physicians to directly select the device and have the FiO2 field auto-filled. In a real clinical setting, the PaI2 value should be obtained by running an arterial blood gas (ABG) through the ABG machine. The FiO2 value is collected by observing the patient’s oxygen device or getting the FiO2 setting from the ventilator. The FiO2 value might not be easily observed if the patient is on a device that does not explicitly state the FiO2. This tool provides a convenient way to access the FiO2 values for these devices. The maximum oxygen flow in this tool is set to 6L as it is recommended that physicians consider using different devices if oxygen requirement is higher."
       articleLink="Link: https://sarah-specialist.github.io/PFcalculator/"
        />
       <Article 
@@ -23,15 +23,15 @@ export default function Portfolio() {
       articleGoal="Goal: This tool helps the general public to easily find and source for available parking."
       articleLF="Languages and Frameworks: HTML, CSS, JavaScript, Axios, API, Modular Programming, Visual Studios Code, GitHub"
       articleFeatures="Features: real carpark data from data.gov.sg API, Using real-time information"
-      articleProcess="Process: The main challenge I faced during this project was to match the input value with a dataset and extract information from that specific dataset."
+      articleProcess="Process: In this assignment, developers were to demonstrate the fetching of data from an API using Axios methods. While the assignment was required only vanilla JavaScript, I was eager to go beyond the scope of the class and create a front-end webpage. However, I encountered the “Cross—Origin Resource Sharing” error because Axios uses XMLHttpRequest under the hood. With research and help from my mentors, I was able to successfully deploy my webpage. "
       articleLink="Link: https://sarah-specialist.github.io/CarparkAvailability/"
       />
       <Article 
       articleTitle="My Official Website"
-      articleGoal="Goal: To create a website that showcase the value of my projects and allow interactions with like-minded individuals."
-      articleLF="Languages and Frameworks: React, React-Router-Dom, HTML, CSS, JavaScript, Visual Studios Code, GitHub"
+      articleGoal="Goal: To create a website that showcase the value of my projects and allow interactions with clients and other developers."
+      articleLF="Languages and Frameworks: React, React-Router-Dom, React Functional Components, EmailJS, HTML, CSS, JavaScript, Visual Studios Code, GitHub"
       articleFeatures="Features: summary of projects, attractive and simple design, react form"
-      articleProcess="Process: The main challenge I faced during this project was to match the input value with a dataset and extract information from that specific dataset."
+      articleProcess="Process: It is important for me to be able to connect with clients and other developers through my website. Since React applications require third-party plugins to send an email from a website form, I looked into using EmailJS to achieve my goals. It worked wonderfully!"
       articleLink="Link: https://sarah-specialist.github.io/my-official-website/"
       />
     </div>
