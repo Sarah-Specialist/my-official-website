08e56e38d1088fbb1273276d2a898897912c4d0e
Sarah Specialist <sarah.specialist@gmail.com>
2022-01-31 11:37:34 +0800

    edited styling

--- official-website/src/components/contactInfo/contactInfo.js
+++ official-website/src/components/contactInfo/contactInfo.js
@@ -3,8 +3,9 @@ import './contactInfo.css';
 
 export default function ContactInfo() {
     return (
-        <div style={{backgroundColor: "inherit"}}>
-            <p style={{backgroundColor: "inherit"}}>Email: sarah.specialist@gmail.com</p>
+        <div className="contactInfo">
+            <p className="information">I would love to hear from you!</p>
+            <p className="information">sarah.specialist@gmail.com</p>
             <p className="directSocials"><Socials /></p>
         </div>
     )
--- official-website/src/components/featured/featured.js
+++ official-website/src/components/featured/featured.js
@@ -5,7 +5,9 @@ export default function Featured() {
     return (
     <div className="featured">
         <h2>Check out my latest project!</h2>
-        <div></div>
+        <div>
+            <p>https://sarah-specialist.github.io/my-official-website/</p>
+        </div>
     </div>
     )
 }
