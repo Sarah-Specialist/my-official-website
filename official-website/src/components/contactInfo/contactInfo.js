32cb83bb81116c75a679ff40d68e31b169cb72b6
Sarah Specialist <sarah.specialist@gmail.com>
2022-02-18 12:17:18 +0800

    minor adjustments

--- official-website/src/components/contactInfo/contactInfo.js
+++ official-website/src/components/contactInfo/contactInfo.js
@@ -4,7 +4,7 @@ import './contactInfo.css';
 export default function ContactInfo() {
     return (
         <div className="contactInfo">
-            <p className="information">I would love to hear from you!</p>
+            <p className="information">I would love to hear from you</p>
             <p className="information">sarah.specialist@gmail.com</p>
             <p className="directSocials"><Socials /></p>
         </div>
--- official-website/src/components/featured/featured.js
+++ official-website/src/components/featured/featured.js
@@ -5,6 +5,7 @@ export default function Featured() {
     return (
     <div className="featured">
         <h2>What's new?</h2>
+        <h3>NEW group project: In-Restaurant Menu App</h3>
         <div>
             <img src="https://i.ibb.co/fqPRsSn/ambrosial.jpg"
             alt="featured" 
